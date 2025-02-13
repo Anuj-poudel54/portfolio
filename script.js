ce37b2b50d913abee5eba2f558da9139aaed886f
Anuj poudel <anujpoudel54@gmail.com>
2025-02-13 12:09:22 +0545

    removed old files

--- script.js
+++
@@ -1,7 +0,0 @@
-
-let r = 240, g = 248, b = 255;
-
-setInterval(() => {
-    r = ((r + 1) % 256 + 230); g = ((g + 1) % 255 + 230);
-    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-}, 1000);
